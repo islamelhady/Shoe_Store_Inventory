92d8276561001d193b1830a98750e8a28f778b41
elhady <i.elhady44@gmail.com>
2022-12-15 06:51:18 +0200

    Hide the back button in the action bar from the screens,using AppBarConfiguration. and add image in welcome screen

--- app/src/main/java/com/elhady/inventory/MainActivity.kt
+++ app/src/main/java/com/elhady/inventory/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.elhady.inventory.databinding.ActivityMainBinding
 
@@ -12,15 +14,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
 
-        setupActionBarWithNavController(navController)
+        /**
+         * Hide the back button in the action bar from the screens, where there is no other fragment to go back to
+         */
+        appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.instructionsFragment,
+            R.id.welcomeFragment,
+            R.id.loginFragment,
+            R.id.shoeListFragment
+        ).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     /**
